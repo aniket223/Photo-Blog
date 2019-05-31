9177a9385e4f73c6214ae3c08b7cd42bf7526503
aniket223 <aniket88885@gmail.com>
2019-05-31 16:29:08 +0530

    append strings to cookie value and split them

--- main.go
+++ main.go
@@ -1,19 +1,20 @@
 package main
 
 import (
+	"github.com/satori/go.uuid"
 	"html/template"
 	"net/http"
-	"github.com/satori/go.uuid"
-
+	"strings"
 )
-type user struct{
-username string
-first string
-last string
+
+type user struct {
+	username string
+	first    string
+	last     string
 }
 
-var dbSessions= map[string]string{}
-var dbUsers= map[string]user{}
+var dbSessions = map[string]string{}
+var dbUsers = map[string]user{}
 var tpl *template.Template
 
 func init() {
@@ -27,18 +28,41 @@ func main() {
 }
 
 func index(w http.ResponseWriter, req *http.Request) {
-	c:=getCookie(w,req) 											//to get cookie in index
-	tpl.ExecuteTemplate(w, "index.gohtml", c)	//executes index.gohtml
+	c := getCookie(w, req)                          //to get cookie in index
+	c = appendValue(w, c)
+	xs := strings.Split(c.Value, "|")
+	tpl.ExecuteTemplate(w, "index.gohtml", xs) //executes index.gohtml with value of cookie
 }
 
-func getCookie(w http.ResponseWriter, req *http.Request) *http.Cookie{		//used to generate cookie
+func getCookie(w http.ResponseWriter, req *http.Request) *http.Cookie { //used to generate cookie
 	c, err := req.Cookie("Session")
-	if err!=nil{
-		sID, _:= uuid.NewV4()
+	if err != nil {
+		sID, _ := uuid.NewV4()
 		c = &http.Cookie{
-			Name:"Session",
-			Value:sID.String(),
+			Name:  "Session",
+			Value: sID.String(),
 		}
 	}
+	return c
+}
+
+func appendValue(w http.ResponseWriter, c *http.Cookie) *http.Cookie {
+	// values
+	p1 := "disneyland.jpg"
+	p2 := "atbeach.jpg"
+	p3 := "hollywood.jpg"
+	// append
+	s := c.Value
+	if !strings.Contains(s, p1) {
+		s += "|" + p1
+	}
+	if !strings.Contains(s, p2) {
+		s += "|" + p2
+	}
+	if !strings.Contains(s, p3) {
+		s += "|" + p3
+	}
+	c.Value = s
+	http.SetCookie(w, c)
 	return c
 }
